f9aab09689ec4d5740dcaf428bc1b9d6d8cf2251
boeckwi <w.boeck@walz.de>
2024-04-05 10:38:29 +0200

    Add project files.

---
+++ SqlIndent/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using PoorMansTSqlFormatterRedux.Formatters;
+using PoorMansTSqlFormatterRedux.Parsers;
+using PoorMansTSqlFormatterRedux.Tokenizers;
+
+string? s;
+var sql = new StringBuilder();
+while ((s = Console.ReadLine()) != null)
+{
+    sql.AppendLine(s);
+}
+
+var tokenizer = new TSqlStandardTokenizer();
+var parser = new TSqlStandardParser();
+var formatter = new TSqlStandardFormatter();
+
+var tokens = tokenizer.TokenizeSQL(sql.ToString());
+var tree = parser.ParseSQL(tokens);
+var formatted = formatter.FormatSQLTree(tree);
+
+Console.WriteLine(formatted);
+
